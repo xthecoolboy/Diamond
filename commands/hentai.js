87263ce13d2a732a8cd3f9ee7e6efe193bfce409
xthecoolboy <67747753+xthecoolboy@users.noreply.github.com>
2020-07-20 16:39:23 +0200

    Update hentai.js

--- commands/hentai.js
+++ commands/hentai.js
@@ -3,7 +3,7 @@ const superagent = require('superagent')
 
 exports.run = (client, msg, args) => {
   if (msg.channel.nsfw === true) {
-    superagent.get('https://nekobot.xyz/api/image')
+    superagent.get('https://nekobot.xyz/api/{?type}')
     .query({ type: 'hentai_food'})
     .end((err, response) => {
       msg.channel.send({ file: response.body.message });
