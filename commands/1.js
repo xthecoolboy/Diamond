7f80ef6c700ee1eb2a3bfcf2ba4633f0cd26a48f
xthecoolboy <67747753+xthecoolboy@users.noreply.github.com>
2020-07-20 16:50:13 +0200

    Update 1.js

--- commands/1.js
+++ commands/1.js
@@ -3,7 +3,7 @@ const superagent = require('superagent')
 
 exports.run = (client, msg, args) => {
   if (msg.channel.nsfw === true) {
-    superagent.get('https://nekobot.xyz/api/image')
+    superagent.get('https://www.google.com/search?q=belle+delphine+photo+website&client=firefox-b-d&sxsrf=ALeKk03MR7u3m-3nzcSjrSzdBdlxWVQk7g:1595256563033&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiFiY_midzqAhWP_qQKHeyEAZQQ_AUoAXoECAwQAw&biw=1366&bih=626')
     .query({ type: 'swimsuit'})
     .end((err, response) => {
       msg.channel.send({ file: response.body.message });
